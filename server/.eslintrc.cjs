aa420e8e998ca1a775df9d0ef3251164890c2160
kmaclip <kmartclips@proton.me>
2026-02-03 11:41:19 -0500

    Add ESLint configs and update tsconfig/package scripts

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'off',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'jest.config.ts'],
+};
